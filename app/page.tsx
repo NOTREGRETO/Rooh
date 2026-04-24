2378c92ad39b5e8a5fd31d68c31e39b34c0b1340
Parth <parth@example.com>
2026-04-24 15:17:26 +0000

    Remove ProjectDetails and Footer sections from page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Marquee from "./components/Marquee";
 import FeatureCards from "./components/FeatureCards";
 import GallerySection from "./components/GallerySection";
 import Members from "./components/Members";
-import ProjectDetails from "./components/ProjectDetails";
-import Footer from "./components/Footer";
+
 import Image from "next/image";
 
 export default function Home() {
@@ -17,8 +16,7 @@ export default function Home() {
       <FeatureCards />
       <GallerySection />
       <Members />
-      <ProjectDetails />
-      <Footer />
+
     </main>
   );
 }
